1c02c032e4ae903c50a7295d652ed38f7fbe7d11
ariovaldo <ariovaldo@ituring.com.br>
2022-09-27 21:21:01 -0300

    criacao projeto e primeiro componente

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import Button from './components/button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button>
+                Botão Teste
+            </button>
+        )
+    }
+}
+
+export default Button;
