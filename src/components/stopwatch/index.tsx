d5c26e36635f5b3043cb2471693ca33b56afacb3
ariovaldo <ariovaldo@ituring.com.br>
2022-09-29 22:49:56 -0300

    introducao ao useEffect

--- src/components/stopwatch/index.tsx
+++ src/components/stopwatch/index.tsx
@@ -3,7 +3,7 @@ import Watch from "./watch";
 import style from "./Stopwatch.module.scss"
 import { timeToSeconds } from "../../common/utils/time";
 import { ITask } from "../../types/task";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
     selected: ITask | undefined
@@ -11,16 +11,18 @@ interface Props {
 
 export default function Stopwatch({selected}: Props) {
     const [time, setTime] = useState<number>();
-    if (selected?.time) {
-        setTime(timeToSeconds(selected.time));
-    }
+
+    useEffect(() => {
+        if (selected?.time) {
+            setTime(timeToSeconds(selected.time))
+        }
+    }, [selected]);
 
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-            Tempo: {time}
             <div className={style.relogioWrapper}>
-                <Watch />
+                <Watch time={time} />
             </div>
             <Button text="Começar!"/>
         </div>
